1cfcef1cd744c267aef313891f3bb52425bf34cb
Wout Bouckaert <woutboat@rams.colostate.edu>
2021-01-22 10:54:29 -0700

    Working fileing

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ Program works to download all of the sequential images
 
 import requests
 import shutil
+import os
 
 # Change this line only
-noaa_url = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/nr/GEOCOLOR" \
-       "/20210200611_GOES16-ABI-nr-GEOCOLOR-2400x2400.jpg"
+noaa_url = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/nr/GEOCOLOR/20210211901_GOES16-ABI-nr-GEOCOLOR-2400x2400.jpg"
 
 # Get num code out of URL
 url_split = noaa_url.split("/")
@@ -18,19 +18,23 @@ url_rest = ""
 for part in url_split[:-1]:
     url_rest += part + "/"
 
-
 file_name_split = start_file_name.split("_")
 number_code = int(file_name_split[0])
 
-file_name = str(number_code) + "_" + file_name_split[1]
-print(url_rest)
-print(number_code)
-print(file_name)
 # Need to write some code that then re-creates the url given the components
 # and the the number_code that was pulled from the url.
 images = 0
 skips = 0
+
+cwd = os.getcwd()
+path = cwd + '/img/'
+
+if not os.path.exists(path):
+    os.makedirs(path)
+
 while images < 250:
+
+    working_url = url_rest + str(number_code) + "_" + file_name_split[1]
 
     # The part where the code doesn't work
     r = requests.get(noaa_url, stream=True)
@@ -39,7 +43,7 @@ while images < 250:
     if r.status_code == 200:
         skips = 0
         r.raw.decode_content = True
-        with open(str(images) + ".jpg", 'a+b') as f:
+        with open(path + str(images) + ".jpg", 'a+b') as f:
             shutil.copyfileobj(r.raw, f)
         print("Downloaded number " + str(images))
         images += 1
