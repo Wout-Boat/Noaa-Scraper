431528e2e2dc317a097492ccbac00be194f88b45
Wout Bouckaert <woutboat@rams.colostate.edu>
2021-02-09 22:56:49 -0700

    Some fancy code

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ https://www.star.nesdis.noaa.gov/goes/index.php
 """
 
 # Imports
-import requests
-import shutil
 import os
+import shutil
+
 import cv2
+import ffmpeg
+
+import requests
 
 # Change this line only
-noaa_url = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/nr/GEOCOLOR/20210390641_GOES16-ABI-nr-GEOCOLOR-2400x2400.jpg"
+noaa_url = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/nr/GEOCOLOR/20210400126_GOES16-ABI-nr-GEOCOLOR-2400x2400.jpg"
 
 # This witchcraft uses Python's insane list splitting tools to select the
 # filename in the URL and set it to start_file_name
@@ -39,7 +42,7 @@ skips = 0
 # Gets the running directory for the script
 cwd = os.getcwd()
 # Adds /img/ to the end of the running directory
-img_path = cwd + '/img/'
+img_path = cwd + '\\img\\'
 
 # Creates the /img/ directory if it doesn't exist
 if not os.path.exists(img_path):
@@ -48,7 +51,7 @@ if not os.path.exists(img_path):
 # Main downloading loop start
 # TODO: need to make the loop not go off of
 #  images but to go just based off of skips.
-while images < 250:
+while True:
 
     # creates the download url for this current loop with the current
     # number code
@@ -59,18 +62,23 @@ while images < 250:
 
     # If file is found
     if r.status_code == 200:
+
         # Reset skips
         skips = 0
 
+        # Required to function but use to me is unknown.
         r.raw.decode_content = True
 
         # Saves image to path
         # zfill(5) adds leading zeros to the numbers, this prevents opencv from
         # ordering the images incorrectly.
         with open(img_path + str(images).zfill(5) + ".jpg", 'a+b') as f:
-            #
+            # save the image data
             shutil.copyfileobj(r.raw, f)
-        print("Downloaded number " + str(images))
+        # Notify user of status
+        if (images % 5 == 0):
+            print("Downloading... (" + str(images) + "/~210)")
+        # Increment images
         images += 1
 
     # If file is not found
@@ -81,26 +89,52 @@ while images < 250:
 
         # If we skip an image too many times kill the loop
         if skips > 15:
-            images += 1000
+            break
 
     # The images are only saved in increments of 5?
     # This skips to the next image in sequence.
     number_code += 5
 
-# TODO: - Need to add coments to the following portion.
-#   - Also need to add a loop here that changes the size of the frames
+# TODO: Need to add a loop here that changes the size of the frames
 #   so that they are 1920x1080
 print("Done downloading files")
 
-video_name = "output.avi"
+print("Putting video in the oven...")
+# Location of the temporary avi video file
+# Following code shamelessly stolen from stack exchange
+video_name = "tmp_output.avi"
+# Makes an list of all the images in /img/
 imgs = [img for img in os.listdir(img_path) if img.endswith(".jpg")]
+
+# Gets the first frame from the list
 frame = cv2.imread(os.path.join(img_path, imgs[0]))
+# Make some nice variables from the frame size
 height, width, layers = frame.shape
 
+# Make the VideoWriter object
 video = cv2.VideoWriter(video_name, 0, 10, (width, height))
 
+# Add all the frames to the video
 for image in imgs:
     video.write(cv2.imread(os.path.join(img_path, image)))
 
+# Bake that bad boy
 cv2.destroyAllWindows()
 video.release()
+
+print("It's out of the oven, watch out it's hot!")
+
+# Convert video
+# TODO: Add comments to following section
+print("Converting video...")
+
+stream = ffmpeg.input("output.avi")
+stream = ffmpeg.output(stream, 'output.mp4')
+ffmpeg.run(stream)
+
+print("Finished converting, cleaning up after myself...")
+os.remove(img_path)
+os.remove("output.avi")
+
+print("All done!")
+print("Your video is in " + cwd + ". As output.mp4!")
